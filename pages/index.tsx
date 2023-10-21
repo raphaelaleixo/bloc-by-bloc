465c24364bcaa9290ab67d9b88ef72f612145874
Raphael Aleixo <raphael.aleixo@gmail.com>
2023-10-20 23:44:00 -0400

    small css changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import { useState, useEffect } from "react";
 import { createNewCity } from "../classes/city/createNewCity";
 import City, { CityBlock } from "../classes/city";
 import District, { Highway } from "../classes/district";
+import { deserialize, serialize } from "class-transformer";
 
 export default function Home() {
   const [city, setCity] = useState<City | undefined>();
@@ -11,19 +12,25 @@ export default function Home() {
     setCity(createNewCity());
   }, []);
 
+  if (city) {
+    const jsonCity = serialize(city);
+    const cityReconstucted = deserialize(City, jsonCity);
+    console.log(cityReconstucted);
+  }
+
   return (
     <>
       <Head>
         <title>Bloc By Bloc</title>
       </Head>
-      <main>
-        <div className="grid grid-cols-5 w-[50vw] gap-1">
+      <main className="contents ">
+        <div className="grid grid-cols-5 w-[50em] gap-4">
           {city?.blocks.map((line: CityBlock[]) =>
             line.map((district: CityBlock) => {
               return (
                 <div
                   key={district.tile?.id}
-                  className="border bg-zinc-900 aspect-square flex flex-col p-2 h-full overflow-hidden relative"
+                  className="bg-zinc-900 aspect-square flex flex-col p-2 h-full overflow-hidden relative"
                   style={{ rotate: `${district.tile.rotation}deg` }}
                 >
                   <div className="text-[0.5rem] text-white font-bold relative z-10">
@@ -33,7 +40,10 @@ export default function Home() {
                     <span>{district.tile?.title}</span>
                   </div>
                   <div className="text-[0.5rem] text-white font-bold relative z-10">
-                    <span>{district.tile instanceof District && district.tile.districtType}</span>
+                    <span>
+                      {district.tile instanceof District &&
+                        district.tile.districtType}
+                    </span>
                   </div>
                   {district.tile instanceof Highway ? (
                     <div></div>
