74c0931467d3fa8198be86ae778460f975887624
raphaelaleixo <raphael.aleixo@gmail.com>
2023-10-29 22:56:09 -0400

    Fix env variable

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  env: {
+    REACT_APP_DATABASE_LINK: process.env.REACT_APP_DATABASE_LINK,
+  },
 }
 
 module.exports = nextConfig
