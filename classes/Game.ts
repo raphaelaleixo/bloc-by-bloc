02b09b1f20005b8d69393e90afe3ab23cdbe6c1f
raphaelaleixo <raphael.aleixo@gmail.com>
2023-11-07 14:00:11 -0500

    Add gameStates

--- classes/Game.ts
+++ classes/Game.ts
@@ -1,3 +1,6 @@
+import { instanceToInstance } from 'class-transformer';
+import { GameStates } from '../utils/constants';
+
 export default class Game {
   players: number;
 
@@ -5,9 +8,20 @@ export default class Game {
 
   room: string;
 
+  state: GameStates = GameStates.Setup;
+
   constructor(players: number, difficulty: number, room: string) {
     this.players = players;
     this.difficulty = difficulty;
     this.room = room;
+  }
+
+  setGameSate(newGameState: GameStates): this {
+    this.state = newGameState;
+    return this;
+  }
+
+  clone(): this {
+    return instanceToInstance(this) as this;
   }
 }
--- utils/constants.ts
+++ utils/constants.ts
@@ -157,3 +157,9 @@ export enum Directions {
   left,
   up,
 }
+
+export enum GameStates {
+  Setup,
+  Night,
+  Day,
+}
